a6ec2e0aa91a4cd96421920fe73d36b975304b6f
zhangyili <927311726@qq.com>
2016-04-14 12:46:34 +0800

    获取手机通讯录

---
+++ GetSystemAddressBook/GetSystemAddressBook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GetSystemAddressBook
+//
+//  Created by 张一力 on 15/10/19.
+//  Copyright © 2015年 张一力. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GetSystemAddressBook/GetSystemAddressBook/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GetSystemAddressBook
+//
+//  Created by 张一力 on 15/10/19.
+//  Copyright © 2015年 张一力. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
